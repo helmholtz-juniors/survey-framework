244f3db9d88d9eafe70310de38c95362e531ce9f
Keno Hassler <40292329+kenohassler@users.noreply.github.com>
2024-11-19 00:00:35 +0100

    polish mental health scoring

--- src/survey_framework/data_analysis/scoring.py
+++ src/survey_framework/data_analysis/scoring.py
@@ -1,7 +1,233 @@
+from enum import StrEnum
+
 import pandas as pd
 
 
-def rate_supervision(
+class Condition(StrEnum):
+    STATE_ANXIETY = "D1"
+    TRAIT_ANXIETY = "D2"
+    DEPRESSION = "D3"
+
+
+def rate_mental_health(
+    responses: pd.DataFrame,
+    condition: Condition,
+    keep_subscores: bool = False,
+) -> pd.DataFrame:
+    """Calculate State/Trait Anxiety or Depression score based on responses to
+        question based on the following references:
+            K. Kroenke, R. L. Spitzer, J. B. W. William, and B. Löwe., The
+                Patient Health Questionnaire somatic, anxiety,and depressive
+                symptom scales: a systematic review. General Hospital
+                Psychiatry, 32(4):345-359, 2010.
+            T. M. Marteau and H. Bekker., The development of a six-item short-
+                form of the state scale of the spielberger state-trait anxiety
+                inventory (STAI). British Journal of Clinical Psychology,
+                31(3):301-306, 1992.
+
+    Args:
+        responses: DataFrame containing responses data
+        condition: Which kind of mental health condition to rate
+        keep_subscores (optional): Whether to include scores from subquestions
+            in the output DataFrame, or only total score and classification.
+            Default False.
+
+    Returns:
+        pd.DataFrame: Mental health condition ratings and classifications
+    """
+
+    # Set up condition-specific parameters
+    match condition:
+        case Condition.STATE_ANXIETY:
+            num_subquestions = 6
+            base_score = 10 / 3
+            conversion = ["pos", "neg", "neg", "pos", "pos", "neg"]
+            label = "state_anxiety"
+            classification_boundaries = [0, 37, 44, 80]
+            classes = ["no or low anxiety", "moderate anxiety", "high anxiety"]
+            # choice_codes = ["A1", "A2", "A3"]
+
+        case Condition.TRAIT_ANXIETY:
+            num_subquestions = 8
+            base_score = 5 / 2
+            conversion = [
+                "pos",
+                "neg",
+                "neg",
+                "pos",
+                "neg",
+                "neg",
+                "pos",
+                "neg",
+            ]
+            label = "trait_anxiety"
+            classification_boundaries = [0, 37, 44, 80]
+            classes = ["no or low anxiety", "moderate anxiety", "high anxiety"]
+            # choice_codes = ["A1", "A2", "A3"]
+
+        case Condition.DEPRESSION:
+            num_subquestions = 8
+            base_score = 1
+            conversion = ["freq" for i in range(8)]
+            label = "depression"
+            classification_boundaries = [0, 4, 9, 14, 19, 24]
+            classes = [
+                "no to minimal depression",
+                "mild depression",
+                "moderate depression",
+                "moderately severe depression",
+                "severe depression",
+            ]
+            # choice_codes = ["A1", "A2", "A3", "A4", "A5"]
+
+    # sanity checks
+    if len(responses.columns) != num_subquestions:
+        raise ValueError(f"expected {num_subquestions} subquestions for {condition}")
+    q_code = responses.columns[0].split("_")[0]
+    if q_code != condition:
+        raise ValueError(f"expected question {condition}, got {q_code}")
+
+    # Set up score conversion dicts
+    pos_direction_scores = {
+        "A1": 4 * base_score,  # "Not at all
+        "A2": 3 * base_score,  # "Somewhat"
+        "A3": 2 * base_score,  # "Moderately
+        "A4": 1 * base_score,  # "Very much"
+    }
+    neg_direction_scores = {
+        "A1": 1 * base_score,  # "Not at all"
+        "A2": 2 * base_score,  # "Somewhat"
+        "A3": 3 * base_score,  # "Moderately"
+        "A4": 4 * base_score,  # "Very much"
+    }
+    frequency_scores = {
+        "A1": 0 * base_score,  # "Not at all"
+        "A2": 1 * base_score,  # "Several days"
+        "A3": 2 * base_score,  # "More than half the days"
+        "A4": 3 * base_score,  # "Nearly every day"
+    }
+    conversion_dicts = {
+        "pos": pos_direction_scores,
+        "neg": neg_direction_scores,
+        "freq": frequency_scores,
+    }
+
+    # Map responses from code to score
+    df = pd.DataFrame()
+    for column, conv in zip(responses.columns, conversion, strict=False):
+        df[f"{column}_score"] = responses[column].map(
+            conversion_dicts[conv], na_action="ignore"
+        )
+
+    # Calculate total anxiety or depression scores
+    # scaled by number of non-NaN responses
+    # e.g. scale by 8/5 if 5/8 subquestions answered
+    responses_counts = df.notna().sum(axis=1)
+    df[f"{label}_score"] = (
+        df.sum(axis=1, skipna=True).div(responses_counts).mul(num_subquestions)
+    )
+
+    # Suppress entries with less than half of all subquestions answered
+    # TODO: we might want to be more strict here
+    df.loc[responses_counts < num_subquestions / 2, f"{label}_score"] = None
+
+    # Classify into categories
+    df[f"{label}_class"] = pd.cut(
+        df[f"{label}_score"],
+        bins=classification_boundaries,
+        labels=classes,
+    )
+
+    if not keep_subscores:
+        df = df.drop(df.columns[:-2], axis=1)
+
+    return df
+
+
+def _rate_satisfaction(
+    question_label: str,
+    responses: pd.DataFrame,
+    choices: dict[str, str],
+    keep_subscores: bool = False,
+) -> pd.DataFrame:
+    """Calculate average overall satisfaction rating
+    Args:
+        question_label (str): Question label to use for transformation type inference
+        responses (pd.DataFrame): DataFrame containing responses data
+        choices (dict): dict for answer choice conversion
+        keep_subscores (bool, optional): Whether to include scores from subquestions
+            in the output DataFrame, or only total score and classification.
+            Default False.
+    Returns:
+        pd.DataFrame: Rounded satisfaction ratings and classifications
+    """
+    # Infer labels from question
+    if "satisfied" in question_label:
+        label = "satisfaction"
+    else:
+        raise ValueError("Question incompatible with specified transformation.")
+    # Satisfation classes sorted from high to low (high score equals high satisfaction)
+    satisfaction_classes = [
+        "very satisfied",
+        "satisfied",
+        "neither/nor",
+        "dissatisfied",
+        "very dissatisfied",
+    ]
+    satisfaction_class_codes = ["A1", "A2", "A3", "A4", "A5"]
+    satisfaction_class_scores = [5.0, 4.0, 3.0, 2.0, 1.0]
+
+    # Set up score conversion dicts for individual questions
+    satisfaction_question_scores = {
+        "Very satisfied": 5.0,
+        "Satisfied": 4.0,
+        "Neither/nor": 3.0,
+        "Dissatisfied": 2.0,
+        "Very dissatisfied": 1.0,
+    }
+    # Inverse satisfaction transformation: Score (5.0) --> Class ('Very satisfied')
+    satisfaction_score_to_class = {
+        score: the_class
+        for the_class, score in zip(
+            satisfaction_classes, satisfaction_class_scores, strict=False
+        )
+    }
+    # Inverse satisfaction transformation: Class ('Very satisfied') --> Code ('A1')
+    satisfaction_class_to_code = {
+        the_class: code
+        for code, the_class in zip(
+            satisfaction_class_codes, satisfaction_classes, strict=False
+        )
+    }
+
+    # Map responses from code to text then to score
+    df = pd.DataFrame()
+    for column in responses.columns:
+        df[f"{column}_score"] = (
+            responses[column]
+            .map(choices)
+            .map(satisfaction_question_scores, na_action="ignore")
+        )
+
+    # Calculate mean rating and round (ignoring NaN)
+    df[f"{label}_score"] = df.mean(axis=1, skipna=True).round()
+
+    # Classify into categories
+    df[f"{label}_class"] = pd.Categorical(
+        df[f"{label}_score"]
+        .map(satisfaction_score_to_class, na_action="ignore")
+        .map(satisfaction_class_to_code, na_action="ignore"),
+        categories=satisfaction_class_codes,
+        ordered=True,
+    )
+
+    if not keep_subscores:
+        df = df.drop(df.columns[:-2], axis=1)
+
+    return df
+
+
+def _rate_supervision(
     question_label: str,
     responses: pd.DataFrame,
     choices: dict[str, str],
@@ -79,247 +305,6 @@ def rate_supervision(
         .map(supervision_score_to_class, na_action="ignore")
         .map(supervision_class_to_code, na_action="ignore"),
         categories=supervision_class_codes,
-        ordered=True,
-    )
-
-    if not keep_subscores:
-        df = df.drop(df.columns[:-2], axis=1)
-
-    return df
-
-
-def rate_mental_health(
-    question_label: str,
-    responses: pd.DataFrame,
-    choices: dict[str, str],
-    condition: str | None = None,
-    keep_subscores: bool = False,
-) -> pd.DataFrame:
-    """Calculate State/Trait Anxiety or Depression score based on responses to
-        question based on the following references:
-            K. Kroenke, R. L. Spitzer, J. B. W. William, and B. Löwe., The
-                Patient Health Questionnaire somatic, anxiety,and depressive
-                symptom scales: a systematic review. General Hospital
-                Psychiatry, 32(4):345–359, 2010.
-            T. M. Marteau and H. Bekker., The development of a six-item short-
-                form of the state scale of the spielberger state-trait anxiety
-                inventory (STAI). British Journal of Clinical Psychology,
-                31(3):301–306, 1992.
-
-    Args:
-        question_label (str): Question label to use for transformation type inference
-        responses (pd.DataFrame): DataFrame containing responses data
-        choices (dict): dict for answer choice conversion
-        condition (str, optional): Which kind of mental health condition to rate,
-            "state_anxiety", "trait_anxiety", or "depression". If not specified,
-            the condition is automatically infered. Default None.
-        keep_subscores (bool, optional): Whether to include scores from subquestions
-            in the output DataFrame, or only total score and classification.
-            Default False.
-
-    Returns:
-        pd.DataFrame: Mental health condition ratings and classifications
-    """
-
-    # Infer condition type if not provided
-    if condition is None:
-        if "I feel calm" in question_label:
-            condition = "state_anxiety"
-        elif "calm, cool and collected" in question_label:
-            condition = "trait_anxiety"
-        elif "interest or pleasure" in question_label:
-            condition = "depression"
-        else:
-            raise ValueError("Question incompatible with any supported condition type.")
-
-    # Set up condition-specific parameters
-    if condition == "state_anxiety":
-        if "I feel calm" not in question_label:
-            raise ValueError("Question incompatible with specified condition type.")
-        num_subquestions = 6
-        base_score = 10 / 3
-        conversion = ["pos", "neg", "neg", "pos", "pos", "neg"]
-        label = "state_anxiety"
-        classification_boundaries = [0, 37, 44, 80]
-        classes = ["no or low anxiety", "moderate anxiety", "high anxiety"]
-        choice_codes = ["A1", "A2", "A3"]
-
-    elif condition == "trait_anxiety":
-        if "calm, cool and collected" not in question_label:
-            raise ValueError("Question incompatible with specified condition type.")
-        num_subquestions = 8
-        base_score = 5 / 2
-        conversion = [
-            "pos",
-            "neg",
-            "neg",
-            "pos",
-            "neg",
-            "neg",
-            "pos",
-            "neg",
-        ]
-        label = "trait_anxiety"
-        classification_boundaries = [0, 37, 44, 80]
-        classes = ["no or low anxiety", "moderate anxiety", "high anxiety"]
-        choice_codes = ["A1", "A2", "A3"]
-    elif condition == "depression":
-        if "interest or pleasure" not in question_label:
-            raise ValueError("Question incompatible with specified condition type.")
-        num_subquestions = 8
-        base_score = 1
-        conversion = ["freq" for i in range(8)]
-        label = "depression"
-        classification_boundaries = [0, 4, 9, 14, 19, 24]
-        classes = [
-            "no to minimal depression",
-            "mild depression",
-            "moderate depression",
-            "moderately severe depression",
-            "severe depression",
-        ]
-        choice_codes = ["A1", "A2", "A3", "A4", "A5"]
-    else:
-        raise ValueError(
-            "Unsupported condition type. "
-            "Please consult your friendly local psychiatrist."
-        )
-
-    # Set up score conversion dicts
-    pos_direction_scores = {
-        "Not at all": 4 * base_score,
-        "Somewhat": 3 * base_score,
-        "Moderately": 2 * base_score,
-        "Very much": 1 * base_score,
-    }
-    neg_direction_scores = {
-        "Not at all": 1 * base_score,
-        "Somewhat": 2 * base_score,
-        "Moderately": 3 * base_score,
-        "Very much": 4 * base_score,
-    }
-    frequency_scores = {
-        "Not at all": 0 * base_score,
-        "Several days": 1 * base_score,
-        "More than half the days": 2 * base_score,
-        "Nearly every day": 3 * base_score,
-    }
-    conversion_dicts = {
-        "pos": pos_direction_scores,
-        "neg": neg_direction_scores,
-        "freq": frequency_scores,
-    }
-    invert_dict = {
-        the_class: code for code, the_class in zip(choice_codes, classes, strict=False)
-    }
-
-    # Map responses from code to text then to score
-    df = pd.DataFrame()
-    for column, conv in zip(responses.columns, conversion, strict=False):
-        df[f"{column}_score"] = (
-            responses[column]
-            .map(choices)
-            .map(conversion_dicts[conv], na_action="ignore")
-        )
-
-    # Calculate total anxiety or depression scores
-    # scaled by number of non-NaN responses
-    # e.g. scale by 8/5 if 5/8 subquestions answered
-    responses_counts = df.notna().sum(axis=1)
-    df[f"{label}_score"] = (
-        df.sum(axis=1, skipna=True).div(responses_counts).mul(num_subquestions)
-    )
-
-    # Suppress entries with less than half of all subquestions answered
-    df.loc[responses_counts < num_subquestions / 2, f"{label}_score"] = None
-
-    # Classify into categories
-    df[f"{label}_class"] = pd.cut(
-        df[f"{label}_score"],
-        bins=classification_boundaries,
-        labels=classes,
-    ).map(invert_dict, na_action="ignore")
-
-    if not keep_subscores:
-        df = df.drop(df.columns[:-2], axis=1)
-
-    return df
-
-
-def rate_satisfaction(
-    question_label: str,
-    responses: pd.DataFrame,
-    choices: dict[str, str],
-    keep_subscores: bool = False,
-) -> pd.DataFrame:
-    """Calculate average overall satisfaction rating
-    Args:
-        question_label (str): Question label to use for transformation type inference
-        responses (pd.DataFrame): DataFrame containing responses data
-        choices (dict): dict for answer choice conversion
-        keep_subscores (bool, optional): Whether to include scores from subquestions
-            in the output DataFrame, or only total score and classification.
-            Default False.
-    Returns:
-        pd.DataFrame: Rounded satisfaction ratings and classifications
-    """
-    # Infer labels from question
-    if "satisfied" in question_label:
-        label = "satisfaction"
-    else:
-        raise ValueError("Question incompatible with specified transformation.")
-    # Satisfation classes sorted from high to low (high score equals high satisfaction)
-    satisfaction_classes = [
-        "very satisfied",
-        "satisfied",
-        "neither/nor",
-        "dissatisfied",
-        "very dissatisfied",
-    ]
-    satisfaction_class_codes = ["A1", "A2", "A3", "A4", "A5"]
-    satisfaction_class_scores = [5.0, 4.0, 3.0, 2.0, 1.0]
-
-    # Set up score conversion dicts for individual questions
-    satisfaction_question_scores = {
-        "Very satisfied": 5.0,
-        "Satisfied": 4.0,
-        "Neither/nor": 3.0,
-        "Dissatisfied": 2.0,
-        "Very dissatisfied": 1.0,
-    }
-    # Inverse satisfaction transformation: Score (5.0) --> Class ('Very satisfied')
-    satisfaction_score_to_class = {
-        score: the_class
-        for the_class, score in zip(
-            satisfaction_classes, satisfaction_class_scores, strict=False
-        )
-    }
-    # Inverse satisfaction transformation: Class ('Very satisfied') --> Code ('A1')
-    satisfaction_class_to_code = {
-        the_class: code
-        for code, the_class in zip(
-            satisfaction_class_codes, satisfaction_classes, strict=False
-        )
-    }
-
-    # Map responses from code to text then to score
-    df = pd.DataFrame()
-    for column in responses.columns:
-        df[f"{column}_score"] = (
-            responses[column]
-            .map(choices)
-            .map(satisfaction_question_scores, na_action="ignore")
-        )
-
-    # Calculate mean rating and round (ignoring NaN)
-    df[f"{label}_score"] = df.mean(axis=1, skipna=True).round()
-
-    # Classify into categories
-    df[f"{label}_class"] = pd.Categorical(
-        df[f"{label}_score"]
-        .map(satisfaction_score_to_class, na_action="ignore")
-        .map(satisfaction_class_to_code, na_action="ignore"),
-        categories=satisfaction_class_codes,
         ordered=True,
     )
 
---
+++ src/survey_framework/tests/test_scoring.py
@@ -0,0 +1,47 @@
+from pathlib import Path
+
+import pytest
+
+from survey_framework.data_analysis.scoring import Condition, rate_mental_health
+from survey_framework.data_import.data_import import LimeSurveyData
+
+
+def test_state_anxiety(survey: LimeSurveyData, output_path: Path) -> None:
+    res_df = rate_mental_health(
+        survey.get_responses(Condition.STATE_ANXIETY),
+        Condition.STATE_ANXIETY,
+        keep_subscores=True,
+    )
+    print(res_df)
+
+
+def test_trait_anxiety(survey: LimeSurveyData, output_path: Path) -> None:
+    res_df = rate_mental_health(
+        survey.get_responses(Condition.TRAIT_ANXIETY),
+        Condition.TRAIT_ANXIETY,
+        keep_subscores=True,
+    )
+    print(res_df)
+
+
+def test_depression(survey: LimeSurveyData, output_path: Path) -> None:
+    res_df = rate_mental_health(
+        survey.get_responses(Condition.DEPRESSION),
+        Condition.DEPRESSION,
+        keep_subscores=True,
+    )
+    print(res_df)
+
+
+def test_invalid_length(survey: LimeSurveyData, output_path: Path) -> None:
+    with pytest.raises(ValueError):
+        rate_mental_health(
+            survey.get_responses(Condition.TRAIT_ANXIETY), Condition.STATE_ANXIETY
+        )
+
+
+def test_invalid_question(survey: LimeSurveyData, output_path: Path) -> None:
+    with pytest.raises(ValueError):
+        rate_mental_health(
+            survey.get_responses(Condition.TRAIT_ANXIETY), Condition.DEPRESSION
+        )
