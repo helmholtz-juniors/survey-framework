e5a7ecb4c39204993386d9c3a1b363f11d157b8b
Kevin <kevin.merchant@helmholtz-munich.de>
2025-08-20 18:44:04 +0200

    Updated with rest of the plots. The histograms made automatically are counts although I want %, so I am manually importing and running them. The plot_bar function in stackedbarplots file has its own rules... So if we want to use the barplot from barplots.py file,it would require quite some changes. If it is possible to adapt just the aesthetics from there, it would be easier. For now it has the old aesthetics.

--- test.py
+++ test.py
@@ -60,7 +60,7 @@ start_year = years_of_phd(survey_data)
 
 ########### Involves Scoring
 
-# ##################################### SA 
+# # ##################################### SA 
 scoring_df = rate_mental_health(responses=survey_data.get_responses("D1"),  condition=Condition.STATE_ANXIETY)
 
 SA_classes = scoring_df.iloc[:,1]
@@ -175,41 +175,56 @@ fig.savefig("Somatic_plot.png", dpi=300)
 # fig.savefig("Burnout_plot.png", dpi=300)
 
 
-# ######################################################################
+######################################################################
 
 
-# ########### Involves Fetching and Plotting 
-# scoring_df= rate_MH_help(survey_data.get_responses("D8"))
-# getting_help_classes = scoring_df.iloc[:,-1]
-# fig,ax = plot_bar(classes=getting_help_classes,n_question = len(getting_help_classes.notna()),
-#                           category_order=["I donot want to answer this question","Yes, I have used them and I was satisfied",
-#                                           "Yes, I have used them, but I was not satisfied","Yes, but I have never used them",
-#                                           "No, I am not aware of any"],
-#                           label_q_data="",
-#                           legend_loc=None) 
-# fig.savefig("GettingHelp_plot.png", dpi=300)
+########### Involves Fetching and Plotting 
+
+#scoring_df= rate_MH_help(survey_data.get_responses("D8"))
+dict_list = survey_data.get_choices("D8")
+# print(dict_list)
+scoring_df = survey_data.get_responses("D8")
+getting_help_classes = (scoring_df.iloc[:,-1]).map(dict_list)
+# print(getting_help_classes.value_counts())
+fig,ax = plot_bar(classes=getting_help_classes,n_question = len(getting_help_classes.notna()),
+                          category_order=['No, I am not aware of any', 'Yes, but I have never used them',
+                         'Yes, I have used them and I was satisfied', 'Yes, I have used them, but I was not satisfied',
+                         'I don’t want to answer this question'],
+                          orientation=Orientation.VERTICAL,
+                          fontsize=20,
+                          label_q_data="",
+                          legend_loc=None) 
+fig.savefig("GettingHelp_plot.png", dpi=300)
 
 
-# scoring_df=survey_data.get_responses("D9")
-# getting_help_2_classes = scoring_df.iloc[:,-2]
-# getting_help_2_classes = getting_help_2_classes.replace(survey_data.get_choices('D9'))
-# fig,ax = plot_bar(classes = getting_help_2_classes,
-#                   n_question = len(getting_help_2_classes.notna()),
-#                   category_order=["Yes","No","I don't want to answer","Other"],
-#                   label_q_data="", 
-#                   legend_loc=None) 
-# fig.savefig("GettingHelp_2_plot.png", dpi=300)
+scoring_df=survey_data.get_responses("D9")
+getting_help_2_classes = scoring_df.iloc[:,-2]
+getting_help_2_classes = getting_help_2_classes.replace(survey_data.get_choices('D9'))
+fig,ax = plot_bar(classes = getting_help_2_classes,
+                  n_question = len(getting_help_2_classes.notna()),
+                  category_order=["Yes","No","I don't want to answer","Other"],
+                  label_q_data="", 
+                  legend_loc=None) 
+fig.savefig("GettingHelp_2_plot.png", dpi=300)
 
-# scoring_df= impact_of_MH(survey_data.get_responses("D6"))
-# MH_impact = scoring_df.iloc[:,-1]
-# fig,ax = plot_bar(classes=MH_impact,n_question = len(MH_impact.notna()),
-#                           category_order=['I dont want to answer this question',
-#                                           'I dont know','I have not been bothered by any problems','Not difficult at all',
-#                                           'Somewhat difficult','Very difficult',
-#                                           'Extremely difficult'],
-#                           label_q_data="",
-#                           legend_loc=None) 
-# fig.savefig("ImpactMH_plot.png", dpi=300)
+
+dict_list = survey_data.get_choices("D6")
+# print(dict_list)
+scoring_df = survey_data.get_responses("D6")
+impact_classes = (scoring_df.iloc[:,-1]).map(dict_list)
+# print(impact_classes.value_counts())
+
+fig,ax = plot_bar(classes=impact_classes,
+                  n_question = len(impact_classes.notna()),
+                          category_order=['I dont want to answer this question',
+                                          'I dont know','I have not been bothered by any problems','Not difficult at all',
+                                          'Somewhat difficult','Very difficult',
+                                          'Extremely difficult'],
+                          orientation=Orientation.VERTICAL,
+                          fontsize=20,
+                          label_q_data="",
+                          legend_loc=None) 
+fig.savefig("ImpactMH_plot.png", dpi=300)
 
 # scoring_df=survey_data.get_responses("D3b")
 # sleep_timings = scoring_df.iloc[:,-1]
